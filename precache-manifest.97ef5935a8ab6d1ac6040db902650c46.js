4a6c381c9c38c12be09457b97b81a7a8fef45d43
Michelet Chavez <michelet@usp.br>
2020-10-22 09:39:20 -0300

    Updates

---
+++ precache-manifest.97ef5935a8ab6d1ac6040db902650c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a635ca83a9eaa68318254b0d79acaf",
+    "url": "/todo-list-v0/index.html"
+  },
+  {
+    "revision": "dc87e7def31ef356716a",
+    "url": "/todo-list-v0/static/css/main.b5a6c479.chunk.css"
+  },
+  {
+    "revision": "515cc310d34fede981ba",
+    "url": "/todo-list-v0/static/js/2.2e0ba4cf.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/todo-list-v0/static/js/2.2e0ba4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc87e7def31ef356716a",
+    "url": "/todo-list-v0/static/js/main.5fb67e44.chunk.js"
+  },
+  {
+    "revision": "a359a2141701d58bca13",
+    "url": "/todo-list-v0/static/js/runtime-main.147a307b.js"
+  }
+]);
